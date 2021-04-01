4493d7ac4308033cd0405802826d5944a8ca87b9
Monadic Cat <greenmonadcat@gmail.com>
2021-04-01 07:25:46 -0500

    note a way to do the thing manually, without the cc crate

--- build.rs
+++ build.rs
@@ -4,6 +4,10 @@ fn main() {
 
     // Removed this in favor of automating building it from the buildscript,
     // using the cc crate which makes this way easy.
+    // You can manually do this by doing
+    //   g++ src/bork.c -c -o src/bork.o
+    //   ar rcs src/libbork.a src/bork.o
+    // and using the two lines below here.
     // println!("cargo:rustc-link-search=./src/");
     // println!("cargo:rustc-flags=-lstdc++");
 }
